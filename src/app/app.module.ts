11dd67146ed381100b581df8d60551f64e55f2ea
Brais Cidras <braisomary@gmail.com>
2018-11-28 10:34:19 +0200

    Routing and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
 
 // Components
 import { AppComponent } from './app.component';
 import { ShopComponent } from './components/shop/shop.component';
 import { ParksComponent } from './components/parks/parks.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { KeepersComponent } from './components/keepers/keepers.component';
+import { AnimalsComponent } from './components/animals/animals.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ShopComponent,
-    ParksComponent
+    ParksComponent,
+    HomeComponent,
+    ContactComponent,
+    KeepersComponent,
+    AnimalsComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
