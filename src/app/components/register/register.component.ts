848d47b16362454097e5fe0489f67a82e187610a
Brais Cidras <braisomary@gmail.com>
2018-12-23 18:16:19 +0200

    Adding user service

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
+import { User } from '../../models/user';
+import { GLOBAL } from '../../services/GLOBAL';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
   public title: String;
+  public user: User;
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _userService: UserService
   ) {
      this.title = 'Sign in';
+     this.user = new User('', '', '', '', '', 'ROLE_USER', '');
   }
 
   ngOnInit() {
     console.log('register.component loaded !!!');
+    console.log(this._userService.register());
+  }
+
+  onSubmit() {
+    console.log(this.user);
   }
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { GLOBAL } from './global';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+  public url: string;
+
+  constructor(private _http: Http) {
+    this.url = GLOBAL.url;
+  }
+
+  register() {
+    return 'Registering new user';
+  }
+}
