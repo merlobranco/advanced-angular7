8dbea3257de36bdf7e2d37aa885d5d92c5769523
Brais Cidras <braisomary@gmail.com>
2018-12-23 16:14:03 +0200

    Adding Models

---
+++ src/app/models/animal.ts
@@ -0,0 +1,9 @@
+export class Animal {
+  constructor(
+    public name: string,
+    public description: string,
+    public year: string,
+    public image: string,
+    public user: string
+  ) { }
+}
