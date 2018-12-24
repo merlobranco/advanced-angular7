c04804e51badc5ff0209651d9e495739518375ff
Brais Cidras <braisomary@gmail.com>
2018-12-24 15:51:03 +0200

    Linking User Object to Login Form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
+import { User } from '../../models/user';
+import { GLOBAL } from '../../services/GLOBAL';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +12,23 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
 export class LoginComponent implements OnInit {
 
   public title: String;
+  public user: User;
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _userService: UserService
   ) {
      this.title = 'Login';
+     this.user = new User('', '', '', '', '', '', '');
   }
 
   ngOnInit() {
     console.log('login.component loaded !!!');
+  }
+
+  onSubmit(registerForm) {
+    console.log(this.user);
   }
 
 }
