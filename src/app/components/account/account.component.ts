4b23758bdfa7753699a8216eece2ed122ea0c669
Brais Cidras <braisomary@gmail.com>
2018-12-26 19:44:11 +0200

    Adding Admin Panel link and solving Warning issues

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../../models/user';
-import { GLOBAL } from '../../services/GLOBAL';
+import { GLOBAL } from '../../services/global';
 import { UserService } from '../../services/user.service';
 import { UploadService } from '../../services/upload.service';
 
